aa0ea5514d74b391023a8e9057e19c32ec58571f
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-10-14 14:57:26 +0200

    cleaned up not authorized

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as sapper from '@sapper/server';
 import compression from 'compression';
-import express from 'express';
-import { auth, requiresAuth } from 'express-openid-connect';
+import express, { Response } from 'express';
+import { auth } from 'express-openid-connect';
 import sirv from 'sirv';
 import authConfig from './authConfig';
 import { connectToDb } from './db';
@@ -11,13 +11,23 @@ const dev = NODE_ENV !== 'production';
 
 connectToDb();
 
+function notAuthorized(res: Response) {
+	res.setHeader('Content-Type', 'application/json');
+	res.status(401).send(JSON.stringify({ error: 'Not authorized' }));
+}
+
 express()
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		auth(authConfig),
-		(req, res, next) =>
-			req.path.startsWith('/api') ? requiresAuth()(req, res, next) : next(),
+		(req, res, next) => {
+			if (req.path.startsWith('/api') && !req['oidc']?.user) {
+				notAuthorized(res);
+			} else {
+				next();
+			}
+		},
 		(req, res, next) =>
 			sapper.middleware({
 				session: () => ({
