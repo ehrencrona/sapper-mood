50ff8c0c6e7b5a731b3c5723314af3ca0498cd00
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-10-14 14:49:25 +0200

    require auth on api routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import sirv from 'sirv';
-import express from 'express';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
+import compression from 'compression';
+import express from 'express';
+import { auth, requiresAuth } from 'express-openid-connect';
+import sirv from 'sirv';
 import authConfig from './authConfig';
 import { connectToDb } from './db';
-import { auth } from 'express-openid-connect';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV !== 'production';
@@ -16,6 +16,8 @@ express()
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		auth(authConfig),
+		(req, res, next) =>
+			req.path.startsWith('/api') ? requiresAuth()(req, res, next) : next(),
 		(req, res, next) =>
 			sapper.middleware({
 				session: () => ({
